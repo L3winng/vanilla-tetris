a51502feebe25f585e7eb5c339eb9906ffa400a6
Basil <basil.gallati@goldinteractive.ch>
2023-02-22 16:25:16 +0100

    added first theming draft, switching with css variables

---
+++ index.js
@@ -0,0 +1,35 @@
+/*
+const buttons = document.querySelectorAll(".theme-switch");
+
+buttons.forEach(button => {
+    button.addEventListener('click', setTheme);
+})
+
+function setTheme($event){
+    document.body.dataset.theme = $event.target.dataset.theme;
+}
+*/
+
+const toggle = document.getElementById("theme-toggle");
+
+
+
+const storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+toggle.onclick = function() {
+    console.log('huhu')
+
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    let targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
+
